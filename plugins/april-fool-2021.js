caed68452d03f1f9c7d5e8b48f0448550021fd04
Frank <25704248+leranjun@users.noreply.github.com>
2021-04-01 21:42:03 +0800

    update(april-fool): IPE v1977 theme (#9)
    LGTM
    
    * Create april-fool-theme.css
    * Update april-fool-2021.js
    * fix(april-fool): bug fix
    * fix(april-fool): typo
    * chore: minor fix
    * chore: prettier
    * chore: minor fix

--- plugins/april-fool-2021.js
+++ plugins/april-fool-2021.js
@@ -1,7 +1,9 @@
 !(() => {
-  if (window.InPageEditAprilFool2021) return
+  if (window.InPageEditAprilFool2021)
+    return console.warn('[InPageEdit] 2021 愚人节源码被多次载入')
   window.InPageEditAprilFool2021 = true
   const isLoaded = localStorage.getItem('InPageEditAprilFool2021')
+
   function showModal() {
     const modal = ssi_modal.createObject({})
     modal.setOptions('center', true)
@@ -20,51 +22,39 @@
   }
 
   const now = new Date()
-  if (isLoaded) {
-    console.log(
-      '[InPageEdit] 2021 愚人节项目已经执行过，再次查看，可以在页面内按顺序输入“ipeaprilfool2021”~'
+  now.setTime(now.getTime() - now.getTimezoneOffset() * 60000)
+
+  if (now.toISOString().startsWith('2021-04-01T')) {
+    mw.loader.load(
+      'https://ipe-plugins.js.org/plugins/april-fool-theme.css',
+      'text/css'
     )
-  } else if (
-    now.getFullYear() === 2021 &&
-    now.getMonth() + 1 === 4 &&
-    now.getDate() === 1
-  ) {
-    localStorage.setItem('InPageEditAprilFool2021', 'true')
-    showModal()
+    if (isLoaded) {
+      console.log(
+        '[InPageEdit] 2021 愚人节项目已经执行过，再次查看，可以在页面内按顺序输入“ipeaprilfool2021”~'
+      )
+    } else {
+      localStorage.setItem('InPageEditAprilFool2021', 'true')
+      showModal()
+    }
   } else {
     console.log(
       '[InPageEdit] 今天不是 2021 愚人节，想要查看，可以在页面内按顺序输入“ipeaprilfool2021”~'
     )
   }
 
+  // ↑↑↓↓←→←→BABA
   !(() => {
     var lastKey = []
     function check(e) {
       lastKey = lastKey || []
       lastKey.push(e.keyCode)
       if (lastKey.length > 16) lastKey.shift()
-      if (/73806965808273767079797650485049$/.test(lastKey.join('')))
+      if (/73806965808273767079797650485049$/.test(lastKey.join(''))) {
         showModal()
+      }
       console.log(lastKey.join(''))
     }
     $(window).on('keydown', check)
   })()
-
-  mw.util.addCSS(`
-.ipe-af-2021 {
-  background: rgba(0, 0, 0, 0.8);
-}
-.ipe-af-2021 iframe {
-  width: 100%;
-  height: 420px;
-  border: 0;
-}
-.ipe-af-2021 .ssi-modalWindow {
-  padding: 0;
-}
-.ipe-af-2021 .ssi-modalContent {
-  margin: 0;
-  padding: 0;
-}
-`)
 })()
