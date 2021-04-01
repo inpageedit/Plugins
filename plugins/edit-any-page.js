bbe369d96ceaede6df722589097b337b36addd64
Dragon-Fish <824399619@qq.com>
2021-04-02 00:07:32 +0800

    fix(i18n): Fixing language pack

--- plugins/edit-any-page.js
+++ plugins/edit-any-page.js
@@ -8,9 +8,9 @@
  */
 mw.hook('InPageEdit').add(({ _msg }) => {
   // Language pack
-  var ipe = window.InPageEdit || {}
+  const ipe = window.InPageEdit || {}
   ipe.i18n = ipe.i18n || {}
-  const toolboxLanguagePack = {
+  const langPack = {
     en: {
       anypage_btn: 'Edit any page',
       anypage_title: 'Quick edit any page',
@@ -22,59 +22,70 @@ mw.hook('InPageEdit').add(({ _msg }) => {
       anypage_label: '请指定页面名',
     },
   }
-  ipe.i18n = $.extend({}, ipe.i18n, toolboxLanguagePack)
-  window.InPageEdit = ipe
+  $.each(langPack, (lang, str) => {
+    ipe.i18n[lang] = ipe.i18n[lang] || {}
+    ipe.i18n[lang] = {
+      ...ipe.i18n[lang],
+      ...str,
+    }
+  })
 
   // toolbox hook
   mw.hook('InPageEdit.toolbox').add(({ $toolbox }) => {
     $toolbox.find('.btn-group.group1').append(
       $('<li>', { class: 'btn-tip-group' }).append(
         $('<div>', { class: 'btn-tip', text: _msg('anypage_btn') }),
-        $('<button>', { class: 'ipe-toolbox-btn fa fa-edit' }).click(function () {
-          ssi_modal.show({
-            className: 'in-page-edit',
-            sizeClass: 'dialog',
-            center: true,
-            outSideClose: false,
-            title: _msg('anypage_title'),
-            content: $('<div>').append(
-              $('<label>').append(
-                $('<b>', { text: _msg('anypage_label') }),
-                $('<br>'),
-                $('<input>', { id: 'which-page', style: 'width: 96%', value: mw.config.get('wgPageName') }).click(function () {
-                  $(this).css('box-shadow', '')
-                })
-              )
-            ),
-            buttons: [
-              {
-                label: _msg('ok'),
-                className: 'btn btn-primary IPE-anypage-ok',
-                keyPress: 13, // Enter
-                method: function (a, modal) {
-                  var page = $('#which-page').val()
-                  if (page === '' || page === undefined) {
-                    $('#which-page').css('box-shadow', '0 0 4px red')
-                    return false
-                  }
-                  modal.close()
-                  InPageEdit.quickEdit({
-                    page: page,
-                    reload: false,
+        $('<button>', { class: 'ipe-toolbox-btn fa fa-edit' }).click(
+          function () {
+            ssi_modal.show({
+              className: 'in-page-edit',
+              sizeClass: 'dialog',
+              center: true,
+              outSideClose: false,
+              title: _msg('anypage_title'),
+              content: $('<div>').append(
+                $('<label>').append(
+                  $('<b>', { text: _msg('anypage_label') }),
+                  $('<br>'),
+                  $('<input>', {
+                    id: 'which-page',
+                    style: 'width: 96%',
+                    value: mw.config.get('wgPageName'),
+                  }).click(function () {
+                    $(this).css('box-shadow', '')
                   })
+                )
+              ),
+              buttons: [
+                {
+                  label: _msg('ok'),
+                  className: 'btn btn-primary IPE-anypage-ok',
+                  keyPress: 13, // Enter
+                  method: function (a, modal) {
+                    var page = $('#which-page').val()
+                    if (page === '' || page === undefined) {
+                      $('#which-page').css('box-shadow', '0 0 4px red')
+                      return false
+                    }
+                    modal.close()
+                    InPageEdit.quickEdit({
+                      page: page,
+                      reload: false,
+                    })
+                  },
                 },
-              },
-              {
-                label: _msg('cancel'),
-                className: 'btn btn-secondary IPE-anypage-cancel',
-                keyPress: 27, // Esc
-                method: function (a, modal) {
-                  modal.close()
+                {
+                  label: _msg('cancel'),
+                  className: 'btn btn-secondary IPE-anypage-cancel',
+                  keyPress: 27, // Esc
+                  method: function (a, modal) {
+                    modal.close()
+                  },
                 },
-              },
-            ],
-          })
-        })
+              ],
+            })
+          }
+        )
       )
     )
   })
