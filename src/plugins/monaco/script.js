bc11cb5ef0b8a821bd3df80211c6a03a3f0c105e
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2024-05-05 15:33:35 +0800

    fix(monaco): compatibility with jQuery < 3.4

--- src/plugins/monaco/script.js
+++ src/plugins/monaco/script.js
@@ -107,10 +107,15 @@ mw.hook('InPageEdit.quickEdit').add(
       }
 
       async function loadScript(src) {
-        return (
-          'monaco' in window ||
-          $.ajax(src, { dataType: 'script', scriptAttrs: { type: 'module' } })
-        )
+        return 'monaco' in window ||
+          new Promise((resolve, reject) => {
+            const s = document.createElement('script')
+            s.type = 'module'
+            s.src = src
+            document.body.appendChild(s)
+            s.addEventListener('load', resolve)
+            s.addEventListener('error', reject)
+          })
       }
 
       function getLangFromContentModel(given = '') {
